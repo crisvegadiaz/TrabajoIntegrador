60ede16fae0ab3fbe427db9290a7afa14f0cbe0a
crisvegadiaz <alexander85vega@gmail.com>
2023-04-06 18:34:47 -0300

    Arreglando el menú

--- script/script.js
+++ script/script.js
@@ -1,26 +1,16 @@
 const botonMenu = document.querySelector(".hamburger"),
   body = document.querySelector("body"),
-  menu = document.querySelector(".cabecera__menu"),
-  enlace =document.querySelector(".menu__enlace");
+  menu = document.querySelector(".cabecera__menu");
 
 // botonMenu hamburger
 botonMenu.addEventListener("click", (e) => {
   botonMenu.classList.toggle("is-active");
   menu.classList.toggle("menuDesativado");
-  
 });
 
-// enlace.addEventListener("click", (e) =>{
-//   menu.classList.toggle("menuDesativado");
-//   botonMenu.classList.toggle("is-active");
-//   console.log("si")
-// })
-
-
-
 body.addEventListener("click", (e) => {
-  if (e.target.classList.contains("menu__enlace")) {
+  if (e.target.classList.contains("enlace__a")) {
     menu.classList.toggle("menuDesativado");
-    console.log("si")
+    console.log("si");
   }
 });
