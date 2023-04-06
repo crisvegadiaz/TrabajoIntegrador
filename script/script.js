7822e1907033bc7088099f7bafecc5e5d92017c3
crisvegadiaz <alexander85vega@gmail.com>
2023-04-05 21:02:34 -0300

    menu responsive

--- script/script.js
+++ script/script.js
@@ -1,7 +1,26 @@
 const botonMenu = document.querySelector(".hamburger"),
-  body = document.querySelector("body");
+  body = document.querySelector("body"),
+  menu = document.querySelector(".cabecera__menu"),
+  enlace =document.querySelector(".menu__enlace");
 
 // botonMenu hamburger
 botonMenu.addEventListener("click", (e) => {
   botonMenu.classList.toggle("is-active");
+  menu.classList.toggle("menuDesativado");
+  
+});
+
+// enlace.addEventListener("click", (e) =>{
+//   menu.classList.toggle("menuDesativado");
+//   botonMenu.classList.toggle("is-active");
+//   console.log("si")
+// })
+
+
+
+body.addEventListener("click", (e) => {
+  if (e.target.classList.contains("menu__enlace")) {
+    menu.classList.toggle("menuDesativado");
+    console.log("si")
+  }
 });
